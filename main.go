f8173c15f1f42945cd57646175a25d1fe74677d2
breeze <644730877@qq.com>
2023-04-04 21:26:38 +0800

    新增dict字典类型数据结构

--- main.go
+++ main.go
@@ -1,10 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"miniRedis/src"
+	"strconv"
 )
 
 func main() {
-	sds := src.NewSDS("hello")
-	println(sds.String())
+	dictType := &src.MyDictType{}
+	dict := src.DictCreate(dictType, nil)
+	for i := 1; i <= 33; i++ {
+		add := dict.DictAdd("key"+strconv.Itoa(i), "val"+strconv.Itoa(i))
+		if add {
+			//println("第"+strconv.Itoa(i), "次添加结果")
+			//dict.Print()
+			//println("------------")
+		}
+		//time.Sleep(500 * time.Millisecond)
+	}
+	value := dict.DictFetchValue("key1")
+	println(fmt.Sprint("before...", value))
+	dict.DictReplace("key1", "newval1")
+	value = dict.DictFetchValue("key1")
+	println(fmt.Sprint("after...", value))
+	dict.DictDelete("key1")
+	value = dict.DictFetchValue("key1")
+	println(fmt.Sprint("delete...", value))
+
 }
