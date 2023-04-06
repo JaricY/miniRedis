439106f1b5b6c0363bddb356ccc5730054e55e2c
JaricY <644730877@qq.com>
2023-04-06 16:30:10 +0800

    添加Robj数据类型

---
+++ datastruct/robj.go
@@ -0,0 +1,26 @@
+package datastruct
+
+type RedisType uint8     //对象类型
+type RedisEncoding uint8 //编码方式
+
+const (
+	RedisTypeString    RedisType = iota
+	RedisTypeHash      RedisType = 1
+	RedisTypeList      RedisType = 2
+	RedisTypeSet       RedisType = 3
+	RedisTypeSortedSet RedisType = 4
+)
+
+const (
+	RedisEncodingInt    RedisEncoding = iota
+	RedisEncodingString RedisEncoding = 1
+	// ...
+)
+
+type RedisObj struct {
+	Type     RedisType
+	Encoding RedisEncoding
+	LRU      int64
+	RefCount int //引用计数
+	Ptr      interface{}
+}
